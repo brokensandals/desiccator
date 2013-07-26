37e4d8d54523500a8b6bf2b2b9cea427d057634f
Jacob Williams <jacobaw@gmail.com>
2013-07-25 23:31:30 -0500

    remove shotgun, fix title, add minimal home page, linkify avatars

--- app/desiccator.rb
+++ app/desiccator.rb
@@ -7,6 +7,10 @@ class Desiccator < Sinatra::Base
   set :database, {adapter: 'sqlite3', database: 'db.sqlite3'}
   set :views, File.dirname(__FILE__) + '/../views'
 
+  get '/' do
+    slim :home
+  end
+
   get '/users/:login/pulls' do
     slim :pulls, locals: {user: User.find_by_login(params[:login])}
   end
