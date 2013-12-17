d4fe76f063a16c38338d11d9562723613a4dcc51
Jacob Williams <jacobaw@gmail.com>
2013-12-16 18:55:16 -0600

    Create separate sections for open and complete reviews

--- models/user.rb
+++ models/user.rb
@@ -3,4 +3,21 @@ class User < ActiveRecord::Base
   has_many :owned_reviews, class_name: 'Review'
   has_many :reviewer_reviews, through: :reviewer_statuses, source: :review
   has_many :repos
+
+  # Reviews this user is a reviewer of, which have not yet been closed.
+  def open_reviewer_reviews
+    reviewer_reviews.where(state: 'open')
+  end
+
+  # Open reviews which this user needs to review.
+  def incomplete_reviews
+    open_reviewer_reviews - complete_reviews
+  end
+
+  # Open reviews this user has finished reviewing.
+  def complete_reviews
+    open_reviewer_reviews.select do |review|
+      review.reviewer_statuses.select(&:completed_at).map(&:user).include?(self)
+    end
+  end
 end
